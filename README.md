66cea6880af4e23a4e36f9e58af53129aed8a43c
devvspaces <netrobepy@gmail.com>
2024-01-23 11:18:34 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Mint NFT 
 
 Follow the instructions on [Alchemy University](https://university.alchemypreview.com/course/ethereum/md/how-to-mint-nfts) to mint your own NFT!
+
+https://ipfs.io/ipfs/QmQnszyhiPi2bNLY3SaacmmZ5Hz4XiFzqcmm1zia92rPuD?filename=cover.png
+
+
+QmQnszyhiPi2bNLY3SaacmmZ5Hz4XiFzqcmm1zia92rPuD
+
+{
+  "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+  "external_url": "https://openseacreatures.io/3", 
+  "image": "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png", 
+  "name": "Dave Starbelly",
+  "attributes": [ ... ]
+}
+
+{
+  path: 'QmZHNRCPAdC5hbzycbwWEqkcyVNgR7pC8KT219trS19Wwy',
+  cid: CID(QmZHNRCPAdC5hbzycbwWEqkcyVNgR7pC8KT219trS19Wwy),
+  size: 529
+}
--- ipfs/upload.js
+++ ipfs/upload.js
@@ -1,36 +1,50 @@
 async function run() {
-    const { create } = await import('ipfs-http-client');
-    const ipfs = await create();
-    
-    // we added three attributes, add as many as you want!
-    const metadata = {
-        path: '/',
-        content: JSON.stringify({
-            name: "My First NFT",
-            attributes: [
-            {
-                "trait_type": "Peace",
-                "value": "10" 
-            },
-            {
-                "trait_type": "Love",
-                "value": "100"
-            },
-            {
-                "trait_type": "Web3",
-                "value": "1000"
-            }
-            ],
-            // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
-            description: "So much PLW3!"
-        })
-    };
+  const { create } = await import("ipfs-http-client");
+  const ipfs = await create();
 
-    const result = await ipfs.add(metadata);
-    console.log(result);
+  // we added three attributes, add as many as you want!
+  const metadata = {
+    path: "/",
+    content: JSON.stringify({
+      name: "BloomByte NFT",
+      attributes: [
+        {
+          trait_type: "Web3",
+          value: "100",
+          max_value: 100,
+        },
+        {
+          trait_type: "Backend Development",
+          value: "100",
+          max_value: 100,
+        },
+        {
+          trait_type: "Frontend Development",
+          value: "100",
+          max_value: 100,
+        },
+        {
+          trait_type: "Application Development",
+          value: "100",
+          max_value: 100,
+        },
+        {
+          display_type: "date",
+          trait_type: "birthday",
+          value: new Date("2024-01-10").getTime(),
+        },
+      ],
+      // update the IPFS CID to be your image CID
+      image:
+        "https://ipfs.io/ipfs/QmQnszyhiPi2bNLY3SaacmmZ5Hz4XiFzqcmm1zia92rPuD",
+      description: "Building the future of Web3 and Artificial Intelligence.",
+    }),
+  };
 
-    process.exit(0);
+  const result = await ipfs.add(metadata);
+  console.log(result);
+
+  process.exit(0);
 }
 
 run();
